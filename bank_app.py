abf3d143a3e2a380e596048cd257b910e0b76393
prateek-pk <35681553+prateek-pk@users.noreply.github.com>
2020-01-18 02:30:50 +0530

    Update bank_app.py

--- bank_app.py
+++ bank_app.py
@@ -1,15 +1,43 @@
+#bank application
+
+#menu
+def menu():
+	print('''----Welcome to AAPNA BANK----
+		--MENU--
+		1. Check Balance
+		2. Mini Statement
+		3. Withdraw cash
+		4. Deposit cash
+		5. Exit''')
+	choice=int(input('--please enter your choice--'))
+
+#running fuctions
+if (choice==1):
+	ac_bal()
+elif (choice==2):
+	min_stat()
+elif (choice==3):
+	withdraw()
+elif (choice==4):
+	deposit()
+elif (choice==5):
+	break
+else:
+	print('NETWORK ERROR!! : PLEASE RETRY')
+	menu()
+
+
 cid=0
 cb=0.0
 cp=0
-ministatement=[cb for i in range(0,count)]
-count=0
+ministatement=[1,2,3,4,5]
 
 def ac_bal():
 	print(cb)
 
 def min_stat():
 	for i in ministatement:
-		print(f"your  transaction:{i}")
+		print(i)
 
 def withdraw():
 	
@@ -24,13 +52,11 @@ def withdraw():
 	
 	else:
 		print('tum gareeb ho')
-	count++
 
 
 def deposit():
-	d=int(input("enter the amount you want to deposit:"))
+	d=int(input("Enter the amount you want to deposit:"))
 
 	ministatement.pop()
 	ministatement.insert(d)
 	cb+=d
-	count++
